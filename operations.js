56daed3e8e2aac9a1d26cea48a62912d785d3471
mohamad hosein <mmdtrni@gmail.com>
2022-12-12 00:06:22 +0330

    operations

---
+++ operations.js
@@ -0,0 +1,19 @@
+const toNum = (s) => {
+    return parseFloat(s)
+}
+
+const addition = (x, y) => {
+    return toNum(x) + toNum(y)
+}
+
+const subtraction = (x, y) => {
+    return toNum(x) - toNum(y)
+}
+
+const multiplication = (x, y) => {
+    return toNum(x) * toNum(y)
+}
+
+const Division = (x, y) => {
+    return toNum(x) / toNum(y)
+}
