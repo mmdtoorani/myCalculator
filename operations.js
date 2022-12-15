ae6c1e44578a2f47ea26bf0ee58b136bff8522b1
mohamad hosein <mmdtrni@gmail.com>
2022-12-15 21:27:53 +0330

    add power and squareRoot function

--- operations.js
+++ operations.js
@@ -17,3 +17,12 @@ const multiplication = (x, y) => {
 const Division = (x, y) => {
     return toNum(x) / toNum(y)
 }
+
+const power = (x, y) => {
+    console.log(toNum(x) ** toNum(y))
+    return toNum(x) ** toNum(y)
+}
+
+const squareRoot = (x, y) => {
+    return toNum(x) ** (1 / toNum(y))
+}
